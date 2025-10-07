6ea346401928253975d2536cb88ec33ea544b41b
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-10-07 17:10:54 -0400

    js/video_player.js

---
+++ js/video_player.js
@@ -0,0 +1,28 @@
+// Load the YouTube IFrame Player API code asynchronously
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var players = {};
+
+function create_video_player(id, vid, delay) {
+  return new YT.Player(id, {
+    height: '608',
+    width: '1080',
+    videoId: vid,
+    playerVars: {
+      playlist: vid, // Enables looping by setting the playlist to the same video ID
+    },
+    events: {
+      onReady: (event) => {
+        event.target.mute(); // Mute the video on ready
+        // event.target.playVideo();
+        event.target.setLoop(true);
+        setTimeout(() => {
+          event.target.playVideo();
+        }, delay * 1000);
+      },
+    },
+  });
+}
